5c1f824843791a1fb596fe669ee601adc2d965b8
sdivanshu582-star <sdivanshu582@gmail.com>
2026-07-30 01:14:55 +0530

    ARRAY

---
+++ array.cpp
@@ -0,0 +1,26 @@
+// MOVE THE ZEROS TO THE END OF THE ARRAY 
+
+//   void moveZeroes(vector<int>& nums) {
+    //:::::::::::::::::::::::::::::::::::::::::// BRUTE-FORCE CODE:::::::::::::::::::::::::::::::::::::
+
+    //      vector<int> temp;
+       
+    //     for (int i=0;i<nums.size();i++){
+    //         if(nums[i]!=0){
+    //             temp.push_back(nums[i]);
+    //         }
+    //     }
+    //     for (int i=0;i<temp.size();i++){
+    //         nums[i]=temp[i];
+            
+    //     }
+    //     for(int i=temp.size();i<nums.size();i++){
+    //         nums[i]=0;
+    //     }
+         
+    // }
+
+    // ::::::::::::::::::::::::::::::::::::::::: OPTIMAL SOLUTION ::::::::::::::::::::::::::::::::::::::
+
+
+    
