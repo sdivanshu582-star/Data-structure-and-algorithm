a19a451370ec51d0801deaa83e3d693ad7999760
sdivanshu582-star <sdivanshu582@gmail.com>
2026-08-03 10:44:11 +0530

    Find the number that appears once

--- array.cpp
+++ array.cpp
@@ -120,3 +120,41 @@
 //             j=i+1;
 //         }
 
+
+
+// :::::::::::::::::::::::::::::::::::::::Find the number that appears once, and other numbers twice.::::::::::::::::::::::::::::::::::::
+
+
+// class Solution {
+// public:
+//     int singleNumber(vector<int>& nums) {
+
+
+/////////////////////////////////////BRUTE FORCE APPROACH///////////////////////////////
+
+//         // int n =nums.size();
+//         // int a;
+//         // for (int i=0;i<=n-1;i++){
+//         //     int count=0;
+//         //     for (int j=0;j<=n-1;j++){
+//         //         if (nums[i]==nums[j]){
+//         //             count ++;
+//         //         }
+//         //     }
+//         //     if(count==1){
+//         //         a=nums[i];
+//         //         break;
+//         //     }
+//         // }
+//         // return a;
+
+// //////////////////////////////////OPTIMAL APPROACH//////////////////////////////////////
+        
+//     // int ans=0;
+//     // for (int i=0;i<=nums.size()-1;i++){
+//     //     ans =ans ^ nums[i];
+//     // }
+//     // return ans;
+
+//     }
+// };
