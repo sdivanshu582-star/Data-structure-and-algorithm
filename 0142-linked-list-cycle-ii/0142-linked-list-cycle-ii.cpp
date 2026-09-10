73455be7517b27b0029fb97c9f531aa70ea5b53b
Divyanshu Singh <sdivanshu582@gmail.com>
2026-09-11 00:04:02 +0530

    Time: 0 ms (100%), Space: 11.4 MB (22.95%) - LeetHub

--- 0142-linked-list-cycle-ii/0142-linked-list-cycle-ii.cpp
+++ 0142-linked-list-cycle-ii/0142-linked-list-cycle-ii.cpp
@@ -13,13 +13,37 @@ public:
 unordered_set<ListNode*>visited;
 ListNode *p=head;
 
-while(p!=NULL){
-if (visited.find(p)!=visited.end()){
-    return p;
+// while(p!=NULL){
+// if (visited.find(p)!=visited.end()){
+//     return p;
+// }
+// visited.insert(p);
+// p=p->next;
+// }
+//      return NULL;  
+
+
+//OPTIMAL SOLUTION
+ListNode* slow=head;
+ListNode* fast=head;
+
+while(fast!=NULL && fast->next!=NULL){
+    
+    slow=slow->next;
+    fast=fast->next->next;
+    if(fast==slow){
+     break;
+    }
 }
-visited.insert(p);
-p=p->next;
+if (fast==NULL || fast->next==NULL){
+    return NULL ;
 }
-     return NULL;  
+//step2->check wheree they meetss
+slow=head;
+while (slow!=fast){
+    slow=slow->next;
+    fast=fast->next;
+}
+return slow;
     }
 };
